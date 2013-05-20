67b2e7980d5b90bbd62a8a4183218f33a2d75700
luannpc00584 <luannp_c00584@fpt.aptech.ac.vn>
2013-05-21 00:06:23 +0700

    Check Commit
    faild

---
+++ Eproject/Eproject.Data/SqlDataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Eproject.Data
+{
+    public class SqlDataProvider
+    {
+        public static string connectionstring = ConfigurationSettings.AppSettings["ConnectionString"].ToString();
+        public static SqlConnection connection;
+        public SqlDataProvider()
+        {
+            if (connection == null)
+            {
+                connection = new SqlConnection(connectionstring);
+            }
+        }
+        public SqlConnection getConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+                return connection;
+            }
+            else
+            {
+                return connection;
+            }
+        }
+    }
+}
